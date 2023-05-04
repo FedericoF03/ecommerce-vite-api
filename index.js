153552e0944005dd51a719f74a08a3986ecfbc2d
FedericoF03 <federicofernandeztec@gmail.com>
2023-05-03 22:34:58 -0300

    Module Integration and Their Use for Variable Control, Cross-Origin-Resource-Policy, Logger, and Cookies

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import Express from "express";
 import Router from "./routes/routes.js";
-
+import morgan from "morgan";
+import CORS from "cors"
+import cookieParser from "cookie-parser";
+import * as dotenv from 'dotenv'
+dotenv.config()
 const app = Express();
 const port = 3005;
+const logger = morgan("tiny");
 
+app.use(CORS({
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+  credentials: true
+}))
+app.use(cookieParser())
+app.use(logger)
 app.use(Router);
 
 app.listen(port, () => console.log(`Running in the port: ${port}`));
