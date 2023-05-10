2c2a5326019239c3bfa924da2e8c13c0d34ec9c1
FedericoF03 <federicofernandeztec@gmail.com>
2023-05-10 00:45:54 -0300

    Middleware to refresh modified access for the API route and database, middleware to receive code in email.

---
+++ src/middlewares/Email.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer"
+const sendEmail = async ({email, generateID}) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: email, // list of receivers
+    subject: "Code Authentication", // Subject line
+    html: `<b>${generateID} is the code for ingress in your auth</b>`, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+export default sendEmail
--- src/middlewares/refreshAcces.js
+++ src/middlewares/refreshAcces.js
@@ -1,9 +1,15 @@
+import UserController from "../controllers/UserController.js";
 import { HTTPConfig } from "../utils/HTTPconfig.js";
 import { cookiesConfig } from "../utils/cookiesConfig.js";
+import { v4 as uuidv4 } from "uuid";
 
 export const refreshAcces = async (req, res, next) => {
   try {
-    if (!req.cookies.access && req.cookies.refresh) {
+    if (
+      !req.cookies.access &&
+      req.cookies.refresh &&
+      req.cookies.refresh.includes("TG")
+    ) {
       HTTPConfig.bodyAccess.body = `grant_type=refresh_token&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&refresh_token=${req.cookies.refresh}`;
       const getRefresh = await fetch(HTTPConfig.urlAccess, HTTPConfig.body);
       const refresh = await getRefresh.json();
@@ -15,9 +21,27 @@ export const refreshAcces = async (req, res, next) => {
       res.cookie("access", refresh.access_token, cookiesConfig);
       cookiesConfig.maxAge = hours.refresh;
       res.cookie("refresh", refresh.refresh_token, cookiesConfig);
+    } else if (req.cookies.refresh && !req.cookies.refresh.includes("TG")) {
+      const user = new UserController();
+      const test = await user.controllerUpdateUser(
+        {
+          acces_token: {
+            token: uuidv4().split("-").join(""),
+            date: new Date(Date.now() + 21600000),
+          },
+        },
+        {
+          $and: [
+            { "acces_token.date": { $lt: new Date() } },
+            { "refresh_token.token": req.cookies.refresh },
+          ],
+        }
+      );
+      console.log(test);
     }
     next();
   } catch (error) {
+    console.log(error);
     return res.status(404).json("no auth");
   }
 };
