accc2144ca1cba4a634ef671649d33ff6ca92a49
FedericoF03 <federicofernandeztec@gmail.com>
2023-05-19 21:44:25 -0300

    The existing controllers were modified for better data readability.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,22 +1,27 @@
 import UserModel from "../models/UserModel.js";
 
 export default class UserController {
-
   controllerCreateUser = async (obj) => {
-    const userModelCreate = new UserModel(obj)
-    const res = await userModelCreate.CreateUser()
-    return res
-  }
+    const userModelCreate = new UserModel(obj);
+    const res = await userModelCreate.CreateUser();
+    return res;
+  };
 
   controllerFindUser = async (obj) => {
-    const userModelFind = new UserModel(obj)
-    const res = await userModelFind.FindUser()
-    return res
-  }
+    const userModelFind = new UserModel(obj);
+    const res = await userModelFind.FindUser();
+    return res;
+  };
 
-  controllerUpdateUser = async (obj, filter) => {
-    const userModelFind = new UserModel(obj,filter)
-    const res = await userModelFind.updateUser()
-    return res
-  }
+  controllerFindUserData = async (obj) => {
+    const userModelFind = new UserModel(obj);
+    const res = await userModelFind.FindUserData();
+    return res;
+  };
+
+  controllerUpdateUser = async (filter, obj, options) => {
+    const userModelFind = new UserModel(obj, filter, options);
+    const res = await userModelFind.updateUser();
+    return res;
+  };
 }
