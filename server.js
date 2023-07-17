77a3a2a15a947352bbae14d3079201c76d23da3f
FedericoF03 <federicofernandeztec@gmail.com>
2023-07-16 21:08:03 -0300

    Changes in the index/server

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 
 import Express from "express";
-import Router from './src/Routes/routes.js';
+import Router from "./src/Routes/routes.js";
 import morgan from "morgan";
-import CORS from "cors"
+import CORS from "cors";
 import cookieParser from "cookie-parser";
-import bodyParser from "body-parser"
-dotenv.config()
+import bodyParser from "body-parser";
+
+import DBConnect, { DBdisconnect } from "./src/middlewares/DB.js";
+dotenv.config();
 const app = Express();
 const port = 3005;
 const logger = morgan("tiny");
 
-app.set('trust proxy', true)
+// app.set("trust proxy", true);
 
-app.use(bodyParser.json())
-app.use(CORS({
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200,
-  credentials: true
-}))
-app.use(cookieParser())
-app.use(logger)
+app.use(bodyParser.json());
+app.use(
+  CORS({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(logger);
+app.use(DBConnect);
 app.use(Router);
+app.use(DBdisconnect)
 app.listen(port, () => console.log(`Running in the port: ${port}`));
